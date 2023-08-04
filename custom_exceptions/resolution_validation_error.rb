f5c34df46474b1c10e58250c4fbe50a1d87c2f55
Iberehovyi <Iberehovyi1655@gmail.com>
2023-08-04 16:47:08 +0300

    Code refactoring.

--- custom_exceptions/resolution_validation_error.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module CustomExceptions
-  class ResolutionValidationError < StandardError
-  end
-end
--- modules/validator.rb
+++ modules/validator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../custom_exceptions/resolution_validation_error'
+require_relative '../exceptions/validation/resolution_error'
+require_relative '../exceptions/validation/year_error'
 module Modules
   module Validator
     DATE_PARAM_COUNT = 6
@@ -18,14 +19,14 @@ module Modules
 
     def validate_month_param_month
       month = month_year[0..1]
-      raise CustomExceptions::MonthValidationError, 'Invalid month specified.' unless month.length == MONTH_PARAM_COUNT
+      raise Exceptions::Validation::MonthError, 'Invalid month specified.' unless month.length == MONTH_PARAM_COUNT
 
       Date.strptime(month, '%m')
     end
 
     def validate_month_param_year
       year = month_year[2..5]
-      raise CustomExceptions::YearValidationError, 'The year is incorrect. The year must be in the format 2023' unless year.length == YEAR_PARAM_COUNT
+      raise Exceptions::Validation::YearError, 'The year is incorrect. The year must be in the format 2023' unless year.length == YEAR_PARAM_COUNT
 
       Date.strptime(year, '%Y')
     end
@@ -33,7 +34,7 @@ module Modules
     def validate_resolution
       resolution_pattern = /^\d{1,4}x\d{1,5}$/
       unless resolution =~ resolution_pattern
-        raise CustomExceptions::ResolutionValidationError, 'No images were found for these parameters, try another parameters.'
+        raise Exceptions::Validation::ResolutionError, 'No images were found for these parameters, try another parameters.'
       end
 
     end
